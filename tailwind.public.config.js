29e530c81e7d914fe9fc50a044e0e4e5b3650179
nexusclimate <jaap@nexusclimate.vc>
2025-12-05 11:39:19 +0400

    V0.12.32
    Erorr handling and meta data

---
+++ tailwind.public.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./public/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
